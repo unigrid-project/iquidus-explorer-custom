aecc461691e5d0b03054ef22b76a8a3b8d276eef
Vixor <vixor@swippcoin.com>
2018-12-04 08:06:36 +0000

    Implement peer refresh functionality into the peers.js script
    This can be invoked by starting the script with "peers.js refresh". The
    operation will loop through all the peers in the database, double checking
    the geoip location and making sure that it is correct.
    
    As IPAPI now seem to be ignoring requests without a user agent field, a
    "iquidus/explorer" user agent header is now added to the request.

--- scripts/peers.js
+++ scripts/peers.js
@@ -17,12 +17,50 @@ dbString = dbString + '@' + settings.dbsettings.address;
 dbString = dbString + ':' + settings.dbsettings.port;
 dbString = dbString + '/' + settings.dbsettings.database;
 
+// displays usage and exits
+function usage() {
+  console.log('Usage: node scripts/sync.js [refresh]');
+  console.log('');
+  console.log('refresh:');
+  console.log('Refresh all and re-fetch all external data for all peers such as');
+  console.log('geolocation and other data.')
+  console.log('');
+  console.log('If no arguments are given, the script will just run and add any');
+  console.log('discovered peers to the database.');
+  console.log('');
+  process.exit(0);
+}
+
+var mode = 'discover';
+
+// check options
+if (process.argv.length > 3) {
+  usage();
+} else if (process.argv.length == 3) {
+  switch(process.argv[2])
+  {
+  case 'refresh':
+    mode = 'refresh';
+    break;
+  default:
+    usage();
+  }
+}
+
+function create_location(geo) {
+  var geolocation = geo.country_name;
+  if (geo.city != null) {
+    geolocation = geo.city + ", " + geo.country_name;
+  }
+  return geolocation;
+}
+
 mongoose.connect(dbString, function(err) {
   if (err) {
     console.log('Unable to connect to database: %s', dbString);
     console.log('Aborting');
     exit();
-  } else {
+  } else if (mode == 'discover') {
     request({uri: 'http://127.0.0.1:' + settings.port + '/api/getpeerinfo', json: true}, function (error, response, body) {
       lib.syncLoop(body.length, function (loop) {
         var i = loop.iteration();
@@ -32,11 +70,9 @@ mongoose.connect(dbString, function(err) {
             // peer already exists or is invalid
             loop.next();
           } else {
-            request({uri: 'https://ipapi.co/' + address + '/json', json: true}, function (error, response, geo) {
-              var geolocation = geo.country_name;
-              if (geo.city != null) {
-                geolocation = geo.city + ", " + geo.country_name;
-              }
+            request({headers: {'User-Agent': 'iquidus/explorer'}, uri: 'https://ipapi.co/' + address + '/json', json: true},
+            function (error, response, geo) {
+              var geolocation = create_location(geo);
               db.create_peer({
                 address: address,
                 protocol: body[i].version,
@@ -50,6 +86,25 @@ mongoose.connect(dbString, function(err) {
         });
       }, function() {
         exit();
+      });
+    });
+  } else if (mode == 'refresh') {
+    // clean up any peers missing geolocation
+    db.get_peers(function(peers) {
+      requests = peers.length;
+      peers.forEach(function(peer, index) {
+        if (peer.geolocation == '') {
+          requests++;
+          request({headers: {'User-Agent': 'iquidus/explorer'}, uri: 'https://ipapi.co/' + peer.address + '/json', json: true},
+          function (error, response, geo) {
+            peer.geolocation = create_location(geo);
+            db.update_peer(peer, function() {
+              requests--;
+            });
+          }, false);
+        }
+        if (index == peers.length - 1)
+          exit();
       });
     });
   }
